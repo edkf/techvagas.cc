89b24ccb9e1c88a64a27590ab745b261fe639071
Edgard Kozlowski <edgardfernandes11@gmail.com>
2020-04-11 12:30:02 -0300

    :pencil2: fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     author: `Edgard Kozlowski`,
     thumbnail: 'src/images/vagascc.png',
     formUrl: 'https://airtable.com/shrcFKFGDq6WcGZkJ',
-    footer: 'Projeto desenvolvido por <a href="https://edkf.com.br" target="_blank">@edkf</a>. Agradecimento especial à <a href="https://kaisermann.me/" target="_blank">@kwistian</a> e <a href="https://www.linkedin.com/in/leilacangussu/" target="_blank">@LeilaCristina</a>.'
+    footer: 'Projeto desenvolvido por <a href="https://edkf.com.br" target="_blank">@edkf</a>. Agradecimento especial à <a href="https://kaisermann.me/" target="_blank">@kiwistian</a> e <a href="https://www.linkedin.com/in/leilacangussu/" target="_blank">@LeilaCristina</a>.'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
