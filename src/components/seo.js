6d88747c47583ec45ba6f3387e40da64a2d99939
Edgard Kozlowski <edgardfernandes11@gmail.com>
2020-04-11 00:32:06 -0300

    :mag: replace home to vagas.cc

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,7 +32,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={title}
+      title={site.siteMetadata.title}
       titleTemplate={site.siteMetadata.title}
       meta={[
         {
@@ -41,7 +41,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:title`,
-          content: title,
+          content: site.siteMetadata.title,
         },
         {
           property: `og:description`,
@@ -61,7 +61,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           name: `twitter:title`,
-          content: title,
+          content: site.siteMetadata.title,
         },
         {
           name: `twitter:description`,
